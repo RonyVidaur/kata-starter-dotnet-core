544dcee3770cf87b58e8ec71f0bc46e7688a5075
RonyVidaur <rony.vidaur@gmail.com>
2019-10-21 14:19:28 -0600

    unknown amount of numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
@@ -6,7 +9,14 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            return int.Parse(s);
+            var numbers = s.Split(",", StringSplitOptions.None);
+            if (numbers.Count() > 1)
+            {
+                var nums = numbers.Select(int.Parse);
+                return nums.First() + nums.Last();    
+            }
+
+            return int.Parse(numbers.First());
         }
     }
 }
