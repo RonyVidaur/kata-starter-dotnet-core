dbbba699287a4fb55ebfb412b4148f070f8755ec
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-11-04 14:19:37 -0600

    custom delimiter failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Net.Sockets;
 
@@ -10,7 +11,7 @@ namespace Kata
       if (string.IsNullOrEmpty(s)) 
         return 0;
 
-      var numbers = s.Split(",").Select(int.Parse);
+      var numbers = s.Split(new[]{",","\n"},StringSplitOptions.None).Select(int.Parse);
 
       return numbers.Sum();
     }
